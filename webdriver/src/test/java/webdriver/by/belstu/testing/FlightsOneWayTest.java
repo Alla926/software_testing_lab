b7dfad5c3bb2033e9195f895aa6fec1b787f8dd4
Alla <alla.grincevich.96@mail.ru>
2019-01-14 00:14:58 +0300

    Changed gitignore, removed page-object

--- webdriver/src/test/java/webdriver/by/belstu/testing/FlightsOneWayTest.java
+++ webdriver/src/test/java/webdriver/by/belstu/testing/FlightsOneWayTest.java
@@ -1,47 +1,52 @@
 package webdriver.by.belstu.testing;
 
+import java.util.Properties;
+
+import org.junit.Assert;
+
 //4) Найти авиарейсы из одного пункта в другой с выбором пункта "в одну сторону" 
 //по указанной дате
 //Ожидаемый результат: отображается список рейсов в одну сторону в указанный день
 
-
-
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class FlightsOneWayTest {
-	private WebDriver driver;
 
-	@Before
-	public void beforeTets() {
-	driver = new ChromeDriver();}
-	
 	@Test
 	public void BrowserToGoogle() {
-		driver.get("https://wizzair.com/ru-ru");
-		WebElement from = driver.findElement(By.xpath("//*[@id=\"search-departure-station\"]"));
+		System.setProperty("webdriver.chrome.driver", "src/resources/driver/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		WebDriverWait wait = new WebDriverWait(driver, 15);
+		driver.manage().window().maximize();
+		driver.get("http://wizzair.com/ru-ru");
+
+		WebElement from = driver.findElement(By.id("search-departure-station"));
 		from.clear();
 		from.sendKeys("Вильнюс\n");
 
-		WebElement to = driver.findElement(By.xpath("//*[@id=\"search-arrival-station\"]"));
+		WebElement to = driver.findElement(By.id("search-arrival-station"));
 		to.clear();
 		to.sendKeys("Барселона\n");
+
+		wait.until(ExpectedConditions
+				.visibilityOfElementLocated(By.xpath("//div[@class='calendar__button']/button[text()=' ОК ']")));
+		WebElement duration = driver.findElement(By.xpath("//div[@class='calendar__button']/button[text()=' ОК ']"));
+		duration.submit();
 		
-		WebElement search = driver.findElement(By.xpath("//*[@id=\"flight-search\"]/div/div/div[3]/form/div[4]/button"));
-		search.submit();
-		System.out.println("Page title is: " + driver.getTitle());
-	}
-	
-	@After
-	public void tearDown() {
+		wait.until(ExpectedConditions
+				.visibilityOfElementLocated(By.xpath("//span[text()='На эту дату нет рейсов.']")));
+		boolean isNotCharts = driver.findElement(By.xpath("//span[text()='На эту дату нет рейсов.']")).isDisplayed();
+		Assert.assertEquals(isNotCharts, true);
+		
 		driver.close();
+		driver = null;
+
 	}
 
 }
